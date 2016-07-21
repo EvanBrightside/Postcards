616c6c486c47c9f51b668b8d6054ec729b925bf2
EvanBrightside <xvanx84@gmail.com>
2016-07-21 23:11:37 +0300

    Add tests for products_controller and product_model

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
   setup do
-    @product = products(:one)
+    @product = products(:hawaii)
       @update = {
         title: 'Greece',
         description: 'Postcard from Greece',
