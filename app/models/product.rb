2ad9e92558d3e709163a0b2b4fd67fbc79bed3ce
EvanBrightside <xvanx84@gmail.com>
2016-07-22 15:42:37 +0300

    Add caching for pages

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,8 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'The image must be JPG, PNG or GIF format'
   }
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
